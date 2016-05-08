9ed331bcd9b6a778d2f4ed051263839a2aa7f6c1
LNJ <lnj.git@gmail.com>
2016-05-08 19:21:37 +0200

    Update tree_growing.lua

--- mods/default/lua/apis/tree_growing.lua
+++ mods/default/lua/apis/tree_growing.lua
@@ -319,16 +319,3 @@ function default.grow_pine_tree(pos)
 	
 	return true
 end
-
---
--- Legacy
---
-
-default.grow_new_apple_tree = default.grow_tree["default:apple"]
-default.grow_new_jungle_tree = default.grow_tree["default:jungle"]
-default.grow_new_pine_tree = default.grow_tree["default:pine"]
-default.grow_new_acacia_tree = default.grow_tree["default:acacia"]
-default.grow_new_birch_tree = default.grow_tree["default:birch"]
-
--- MTG compatibility
-default.grow_new_aspen_tree = default.grow_tree["default:birch"]
